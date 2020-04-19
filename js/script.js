b7e9ed489ec4f788f31080416327e6d3278f9f3c
Ajay <majay1638@gmail.com>
2020-04-19 16:01:55 +0530

    refatored code

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@ document.querySelector('.fa-times').addEventListener('click', function () {
     document.querySelector('.navigation').classList.toggle('open-menu');
 
 });
+const date = new Date().getFullYear();
+const copyRightP = document.querySelector('.footer') ;
+copyRightP.innerText += ` ${date}`;
+
 
 document.querySelector('.fa-bars').addEventListener('click', function () {
     document.querySelector('.fa-bars').classList.toggle('open-menu');
     document.querySelector('.fa-times').classList.toggle('close-menu');
     document.querySelector('.navigation').classList.toggle('open-menu');
-
 
 });
 //document.querySelector('.fa-times').classList.toggle('close-menu');
